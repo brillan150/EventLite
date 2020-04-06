7d60e09081d9e0228bbdafeaf02742d1e88d7c41
Brillan <46096098+bpm150@users.noreply.github.com>
2020-04-05 19:31:14 -0700

    Put ConnectionString in appsettings.json Inject the DbContextOptionsBuilder for the CatalogContext into the services (in ConfigureServices) Fixed error about braces in notes about appsettings.json

--- EventLite/Startup.cs
+++ EventLite/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EventCatalogApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,14 @@ namespace EventLite
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Where? For creating CatalogContext (when the runtime does it)
+            // The DbContextOptions to build needs to say that the where
+            // is a sql server, specifically, this sql server that is specified
+            // in the appsettings.json by "ConnectionString"
+            services.AddDbContext<CatalogContext>(builder =>
+                builder.UseSqlServer(Configuration["ConnectionString"]));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
