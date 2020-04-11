e79f8135edf3b1b296986aeaef2309a4a2ce326f
dreyerz <59781071+dreyerz@users.noreply.github.com>
2020-04-11 14:46:45 -0700

    Commit for Pic, Formt, and Topic APIs, as well as Seed Data and pictures

--- EventLite/Controllers/EventsController.cs
+++ EventLite/Controllers/EventsController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EventCatalogApi.Data;
-using EventCatalogApi.ViewModels;
 using EventLite.Domain.EventLite;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -22,35 +21,6 @@ namespace EventCatalogApi.Controllers
         {
             _context = context;
             _config = config;
-        }
-
-        [HttpGet]
-        [Route("[action]")]
-        public async Task<IActionResult> Event([FromQuery]int pageIndex = 0, [FromQuery]int pageSize = 3)
-        {
-            var itemsCount = await _context.CatalogEvents.LongCountAsync();
-            var events = await _context.CatalogEvents
-                .Skip(pageIndex * pageSize)
-                .Take(pageSize)
-                .ToListAsync();
-
-            events = ChangePictureUrl(events);
-
-            var model = new PaginatedEventsVM<CatalogEvent>
-            {
-                PageIndex = pageIndex,
-                PageSize = pageSize,
-                Count = itemsCount,
-                Data = events
-            };
-            return Ok(model);
-
-        }
-
-        private List<CatalogEvent> ChangePictureUrl(List<CatalogEvent> events)
-        {
-            events.ForEach(e => e.PictureUrl = e.PictureUrl.Replace("http://externalcatalogbaseurltobereplaced/api/pic/1", _config["ExternalCatalogBaseUrl"]));
-            return events;
         }
 
         [HttpGet]
